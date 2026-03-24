06021191914207c154644d97b038c96b05c83634
govindgcoder <govind.g.coder@gmail.com>
2026-03-24 19:25:17 +0530

    test

--- app/src/main/java/org/markdwn/MainApp.java
+++ app/src/main/java/org/markdwn/MainApp.java
@@ -1,35 +1,32 @@
 package org.markdwn;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 // all imports
 
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.web.WebView;
 import javafx.stage.DirectoryChooser;
-import java.io.File;
 import javafx.stage.Stage;
-import javafx.scene.layout.BorderPane;
-
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.Files;
-
-
 
 public class MainApp extends Application {
-    
+
     private Path dirPath;
-    
-    private void loadDirectory(Path dirPath, ListView<String> sideBar){
+
+    private void loadDirectory(Path dirPath, ListView<String> sideBar) {
         sideBar.getItems().clear();
-        
+
         File folder = dirPath.toFile();
         File[] files = folder.listFiles();
-        
+
         if (files == null) return;
-        
+
         for (File file : files) {
             if (file.isFile() && file.getName().toLowerCase().endsWith(".md")) {
                 sideBar.getItems().add(file.getName());
@@ -47,16 +44,18 @@ public class MainApp extends Application {
 
         // to display formatted
         WebView output = new WebView();
-        
+
         // for selecting folder we use DirectoryChooser
         DirectoryChooser directoryChooser = new DirectoryChooser();
         directoryChooser.setTitle("Select a Directory");
-    
+
         // for set initial directory
-        directoryChooser.setInitialDirectory(new File(System.getProperty("user.home")));
-    
+        directoryChooser.setInitialDirectory(
+            new File(System.getProperty("user.home"))
+        );
+
         File selectedDirectory = directoryChooser.showDialog(stage);
-    
+
         if (selectedDirectory != null) {
             dirPath = Paths.get(selectedDirectory.getAbsolutePath());
             loadDirectory(dirPath, sideBar);
@@ -64,7 +63,7 @@ public class MainApp extends Application {
             // System.out.println("No directory selected");
             Platform.exit();
         }
-        
+
         // for the appbar
         ToolBar appBar = new ToolBar(
             new Label("File name"),
@@ -83,30 +82,33 @@ public class MainApp extends Application {
                 output // Column 3
             );
 
-        splitPane.setDividerPositions(0.15, 0.7); 
-        
+        splitPane.setDividerPositions(0.15, 0.7);
+
         // for reading selected file
-        sideBar.getSelectionModel().selectedItemProperty().addListener((obs, oldVal, newVal) -> {
-            if (newVal != null) {
-                try {
-                    // select the file
-                    Path filePath = dirPath.resolve(newVal);
-                    // get content
-                    String content = Files.readString(filePath);
-                    input.setText(content);
-                } catch (Exception ex) {
-                    ex.printStackTrace();
+        sideBar
+            .getSelectionModel()
+            .selectedItemProperty()
+            .addListener((obs, oldVal, newVal) -> {
+                if (newVal != null) {
+                    try {
+                        // select the file
+                        Path filePath = dirPath.resolve(newVal);
+                        // get content
+                        String content = Files.readString(filePath);
+                        input.setText(content);
+                    } catch (Exception ex) {
+                        ex.printStackTrace();
+                    }
                 }
-            }
-        });
-        
+            });
+
         // for main layout:
         BorderPane root = new BorderPane();
         root.setTop(appBar);
         root.setCenter(splitPane);
 
         // Scene is container for all elements
-        Scene scene = new Scene(root, 1200, 600); 
+        Scene scene = new Scene(root, 1200, 600);
         stage.setTitle("Markdwn");
         stage.setScene(scene);
         stage.show();
