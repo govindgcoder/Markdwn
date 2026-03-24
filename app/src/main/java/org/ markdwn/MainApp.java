fb32f3beccda7a56d95a1d413b6a56a3f2d8cc9b
govindgcoder <govind.g.coder@gmail.com>
2026-03-24 18:19:51 +0530

    basic file system

--- app/src/main/java/org/ markdwn/MainApp.java
+++ app/src/main/java/org/ markdwn/MainApp.java
@@ -3,26 +3,42 @@ package org.markdwn;
 // all imports
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.web.WebView;
+import javafx.stage.DirectoryChooser;
+import java.io.File;
 import javafx.stage.Stage;
 import javafx.scene.layout.BorderPane;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.Files;
+
+
 
 public class MainApp extends Application {
+    
+    private Path dirPath;
+    
+    private void loadDirectory(Path dirPath, ListView<String> sideBar){
+        sideBar.getItems().clear();
+        
+        File folder = dirPath.toFile();
+        File[] files = folder.listFiles();
+        
+        if (files == null) return;
+        
+        for (File file : files) {
+            if (file.isFile() && file.getName().toLowerCase().endsWith(".md")) {
+                sideBar.getItems().add(file.getName());
+            }
+        }
+    }
 
     @Override
     public void start(Stage stage) {
-        // for the appbar
-        ToolBar appBar = new ToolBar(
-            new Label("File name"),
-            new Separator(),
-            new Button("New"),
-            new Separator(),
-            new Button("Save")
-        );
-
         // sideBar
         ListView<String> sideBar = new ListView<>();
 
@@ -31,6 +47,32 @@ public class MainApp extends Application {
 
         // to display formatted
         WebView output = new WebView();
+        
+        // for selecting folder we use DirectoryChooser
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+        directoryChooser.setTitle("Select a Directory");
+    
+        // for set initial directory
+        directoryChooser.setInitialDirectory(new File(System.getProperty("user.home")));
+    
+        File selectedDirectory = directoryChooser.showDialog(stage);
+    
+        if (selectedDirectory != null) {
+            dirPath = Paths.get(selectedDirectory.getAbsolutePath());
+            loadDirectory(dirPath, sideBar);
+        } else {
+            // System.out.println("No directory selected");
+            Platform.exit();
+        }
+        
+        // for the appbar
+        ToolBar appBar = new ToolBar(
+            new Label("File name"),
+            new Separator(),
+            new Button("New"),
+            new Separator(),
+            new Button("Save")
+        );
 
         SplitPane splitPane = new SplitPane();
         splitPane
@@ -42,6 +84,21 @@ public class MainApp extends Application {
             );
 
         splitPane.setDividerPositions(0.15, 0.7); 
+        
+        // for reading selected file
+        sideBar.getSelectionModel().selectedItemProperty().addListener((obs, oldVal, newVal) -> {
+            if (newVal != null) {
+                try {
+                    // select the file
+                    Path filePath = dirPath.resolve(newVal);
+                    // get content
+                    String content = Files.readString(filePath);
+                    input.setText(content);
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
         
         // for main layout:
         BorderPane root = new BorderPane();
